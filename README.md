7d7a242ba86e90e3952bd65726932ee9695e7207
e <lambert1129@foxmail.com>
2018-05-14 11:40:28 +0800

    第二次提交

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # slides-demo-2
 
-<p>第一次提交代码；</p>
+<p>第一次提交代码:</p>
 <ol>
     <li>三张图片无缝轮播</li>
     <li>代码优化</li>
+</ol>
+
+<p>第二次提交代码:</p>
+<ol>
+    <li>可任意增加多张图无缝轮播</li>
+    <li>增加自动播放</li>
+    <li>增加mouseenter、mouseleave</li>
+    <li>增加自动播放</li>
+    <li>增加上一张、下一张功能</li>
+    <li>解决页面hidden的Bug</li>
 </ol>
--- main.js
+++ main.js
@@ -10,8 +10,39 @@ $slides.css({transform:'translateX(-400px)'})
 
 bindEvents();
 
+//鼠标点击上一张下一张
+$('#next').on('click',function(){
+    goToSlide(current+1)
+})
 
+$('#previous').on('click',function(){
+    goToSlide(current-1)
+})
 
+//计时器，自动播放，同时解决页面hidde的Bug
+document.addEventListener("visibilitychange",function(e){
+    if(document.hidden){
+        window.clearInterval(timer)
+    }else{
+        timer = setInterval(function(){
+            goToSlide(current+1)
+        },2000)
+    }
+})
+
+let timer = setInterval(function(){
+    goToSlide(current+1)
+},2000)
+
+$('.container').on('mouseenter',function(){
+    window.clearInterval(timer)
+})
+
+$('.container').on('mouseleave',function(){
+    timer = setInterval(function(){
+        goToSlide(current+1)
+    },2000)
+})
 
 
 /******************* */
@@ -23,52 +54,49 @@ function makeFakeSlides(){
 }
 
 function bindEvents(){
-    $buttons.eq(0).on('click',function(){
-        if(current===2){
-            //说明你是从最后一张到第一张
-            $slides.css({
-                transform:'translateX(-1600px)'
-            })
-            .one('transitionend',function(){
-                $slides.hide()
-                .offset()//这是小技巧，hide() show()之间插入offset()，阻断浏览器同时执行
-                $slides.css({transform:'translateX(-400px)'})
-                .show()
-            })   
-        }else{
-            $slides.css({
-                transform:'translateX(-400px)'
-            })
-        }
-        current = 0;
+    $('#buttons').on('click','button',function(e){
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
     })
-    
-    $buttons.eq(1).on('click',function(){
-        $slides.css({
-            transform:'translateX(-800px)'
-        })
-        current = 1;
-    })
-    
-    $buttons.eq(2).on('click',function(){
-        if(current===0){
-            //说明你是从第一张到最后一张
-            $slides.css({
-                transform:'translateX(0px)'
-            })
-            .one('transitionend',function(){
-                $slides.hide()
-                .offset()
-                $slides.css({transform:'translateX(-1200px)'})
-                .show()
-            })  
-            
-        }else{
-            $slides.css({
-                transform:'translateX(-1200px)'
-            })       
-        }
-        current = 2;
-    })
-
 }
+
+function goToSlide(index){
+
+    if(index>$buttons.length-1){
+        index=0
+
+    }else if(index<0){
+        index=$buttons.length-1
+    }
+
+    if(current === $buttons.length-1 && index === 0){
+        //最后一张到第一张
+        $slides.css({
+            transform:`translateX(${-($buttons.length+1)*400}px)`
+        })
+        .one('transitionend',function(){
+            $slides.hide()
+            .offset()//这是小技巧，hide() show()之间插入offset()，阻断浏览器同时执行
+            $slides.css({transform:'translateX(-400px)'})
+            .show()
+        })   
+    }else if(current === 0 && index === $buttons.length-1){
+        //第一张到最后一张
+        $slides.css({
+            transform:'translateX(0px)'
+        })
+        .one('transitionend',function(){
+            $slides.hide()
+            .offset()
+            $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+            .show()
+        })  
+    }else{
+        $slides.css({
+            transform:`translateX(${-(index+1)*400}px)`
+        })
+    }
+    current = index
+}
+
